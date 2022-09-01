e063cf49caf8b2d11752590357b07456bf7ec61b
chuspita <kosecki99@gmail.com>
2022-09-01 17:36:42 +0200

    sm6150-common:sparkify

--- sm6150.mk
+++ sm6150.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-spark
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
